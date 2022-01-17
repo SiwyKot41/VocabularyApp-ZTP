43923ed5517c90c8f46449254fd8f6a9e323a640
SiwyKot41 <Frutkowski80@gmail.com>
2022-01-17 14:45:39 +0100

    Add very basic menu

--- src/main/java/com/example/vocabularyappztp/HelloApplication.java
+++ src/main/java/com/example/vocabularyappztp/HelloApplication.java
@@ -8,10 +8,16 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    private MenuController menuController;
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+
+        menuController = fxmlLoader.<MenuController>getController();
+        menuController.initialize();
+
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
