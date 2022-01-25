4d5b060e6077c59c5506fc447ea6c9246cd47ad2
patrycjarecko <patrycjarecko00@gmail.com>
2022-01-25 12:31:03 +0100

    feat: add back button in choose mode view

--- src/main/java/com/example/vocabularyappztp/controllers/ChooseModeController.java
+++ src/main/java/com/example/vocabularyappztp/controllers/ChooseModeController.java
@@ -37,4 +37,17 @@ public class ChooseModeController {
     public void initialize(Stage stage) {
         this.stage = stage;
     }
+
+    public void onClickBackButton(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(VocabularyApplication.class.getResource("menu-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+
+        MenuController menuController = fxmlLoader.<MenuController>getController();
+        menuController.initialize(stage);
+
+        stage.setScene(scene);
+        stage.setHeight(436.0);
+        stage.setWidth(655.0);
+        stage.show();
+    }
 }
